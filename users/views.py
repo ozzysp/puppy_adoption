b596a765f780a44980cc1c525757f461dee468b2
Ozzy Scuba <ozzysp@icloud.com>
2023-02-28 02:45:10 -0300

    Update views.py file

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def crud(request):
@@ -12,9 +14,11 @@ def crud(request):
         password = request.POST.get('password')
         retype = request.POST.get('retype')
         if len(name.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0 or len(retype.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Fill all fields')
             return render(request, 'crud.html')
 
         if password != retype:
+            messages.add_message(request, constants.WARNING, 'Passwords did not match')
             return  render(request, 'crud.html')
        
 
@@ -24,8 +28,10 @@ def crud(request):
                 email=email,
                 password=password
             )
-            return render(request, 'crud.html')
+            messages.add_message(request, constants.SUCCESS, 'Account created successfully')
+            return render(request, 'login.html')
         except:
+            messages.add_message(request, constants.ERROR, 'SYSTEM ERROR')
             return render(request, 'crud.html')
 
 
