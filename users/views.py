78ce7d3092daa1cd6b6921b6fa1fd792134cdd01
Ozzy Scuba <ozzysp@icloud.com>
2023-02-28 00:46:12 -0300

    Updadte views.py file

--- users/views.py
+++ users/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 
 
 def crud(request):
-    return render(request, 'crud.html')
+    if request.method == 'GET':
+        return render(request, 'crud.html')
+    else:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        retype = request.POST.get('retype')
+        return HttpResponse('TESTE TESTE')
+
 
 
 def login(request):
