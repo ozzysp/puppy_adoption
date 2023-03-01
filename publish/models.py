05c4e19174dcebe9fd0f3dce7fcb43a7552b8c10
Ozzy Scuba <ozzysp@icloud.com>
2023-03-01 00:08:16 -0300

    Make migrations

--- publish/models.py
+++ publish/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tag
+
+
+class Race(models.Model):
+    race = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.race
+
+
+class Pet(models.Model):
+    choices_status = (('F', 'For adoption'),
+                      ('A', 'Adoptated'))
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    picture = models.ImageField(upload_to="pix_puppies")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag)
+    race = models.ForeignKey(Race, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=choices_status, default='P')
+
+    def __str__(self):
+        return self.name
--- users/views.py
+++ users/views.py
@@ -1,62 +1,62 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
-from django.contrib.auth import authenticate, login as dj_login, logout
+from django.contrib.auth import authenticate, logout, login as dj_login
 from django.shortcuts import redirect
 
 
 def crud(request):
     if request.user.is_authenticated:
-        return HttpResponse('Seria o CRUD')
-        if request.method == 'GET':
+        return redirect('/auth/publish')
+    if request.method == 'GET':
+        return render(request, 'crud.html')
+    else:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        retype = request.POST.get('retype')
+        if len(name.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0 or len(retype.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Fill all fields')
             return render(request, 'crud.html')
-        else:
-            name = request.POST.get('name')
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            retype = request.POST.get('retype')
-            if len(name.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0 or len(retype.strip()) == 0:
-                messages.add_message(request, constants.ERROR, 'Fill all fields')
-                return render(request, 'crud.html')
 
-            if password != retype:
-                messages.add_message(request, constants.WARNING, 'Passwords did not match')
-                return  render(request, 'crud.html')
-        
-            try:  
-                user = User.objects.create_user(
-                    username=name,
-                    email=email,
-                    password=password
-                )
-                messages.add_message(request, constants.SUCCESS, 'Account created successfully')
-                return render(request, 'login.html')
-            except:
-                messages.add_message(request, constants.ERROR, 'SYSTEM ERROR')
-                return render(request, 'crud.html')
+        if password != retype:
+            messages.add_message(request, constants.WARNING, 'Passwords did not match')
+            return  render(request, 'crud.html')
+    
+        try:  
+            user = User.objects.create_user(
+                username=name,
+                email=email,
+                password=password
+            )
+            messages.add_message(request, constants.SUCCESS, 'Account created successfully')
+            return render(request, 'login.html')
+        except:
+            messages.add_message(request, constants.ERROR, 'SYSTEM ERROR')
+            return render(request, 'crud.html')
 
 
 
 def login(request):
     if request.user.is_authenticated:
-        return HttpResponse('Seria a pagina de entrada')
-        if request.method == 'GET':
+        return redirect('/auth/publish')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')           
+        user = authenticate(username=name,
+                            password=password)
+        if user is not None:
+            dj_login(request, user)
+            return redirect('/publish/new_puppy')
+        else:
+            messages.add_message(request, constants.ERROR, 'Unregistered user')
             return render(request, 'login.html')
-        elif request.method == 'POST':
-            name = request.POST.get('name')
-            password = request.POST.get('password')           
-            user = authenticate(username=name,
-                                password=password)
-            if user is not None:
-                dj_login(request, user)
-                return redirect('/publish/new_puppy')
-            else:
-                messages.add_message(request, constants.ERROR, 'Unregistered user')
-                return render(request, 'login.html')
 
 
-def logout(request):
+def logout_user(request):
     logout(request)
-    return redirect('/auth/login')
+    return redirect('auth/login')
