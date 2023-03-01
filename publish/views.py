c1fe1ae6e3e3a29a38baafdfccabd8dd12375c83
Ozzy Scuba <ozzysp@icloud.com>
2023-03-01 10:39:41 -0300

    Fix minors errors

--- publish/views.py
+++ publish/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def new_puppy(request):
+    if request.method == "GET":
+        return render(request, "new_puppy.html")
--- puppy_adoption/urls.py
+++ puppy_adoption/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
-    #path('publish/', include('publish.urls')),
+    path('publish/', include('publish.urls')),
 ]
