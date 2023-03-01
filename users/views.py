8d52f824222a357da23179ac575292254ac9ce3f
Ozzy Scuba <ozzysp@icloud.com>
2023-03-01 11:56:34 -0300

    Update views.py file

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
